bab9a704a67d9ded8998d3edf553ce3ed1160e89
Nikola Milikic <nikola.milikic@gmail.com>
2018-12-21 19:27:10 +0100

    Promenjen naziv klase koja poziva ispravku koda.

--- src/ispravka_koda/ProbaStringOperacije.java
+++ src/ispravka_koda/ProbaStringOperacije.java
@@ -1,6 +1,6 @@
 package ispravka_koda;
 
-public class TestStringOperacije {
+public class ProbaStringOperacije {
 
 	public static void main(String[] args) {
 		String[] niz = new String[] { "OKO", "I JOGURT UJUTRU GOJI", "RUDAR", "ZUTO", "ANA VOLI MILOVANA" };
