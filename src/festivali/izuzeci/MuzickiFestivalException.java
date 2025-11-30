670bf18e6dae30d6937dda66c89d4f2662bc9f8a
PetarLakcevic <petar.lakcevic@gmail.com>
2025-11-30 16:29:58 +0100

    Update materials

---
+++ src/festivali/izuzeci/MuzickiFestivalException.java
@@ -0,0 +1,10 @@
+package festivali.izuzeci;
+
+public class MuzickiFestivalException extends RuntimeException {
+
+	private static final long serialVersionUID = -4480428061751452131L;
+
+	public MuzickiFestivalException(String message) {
+		super(message);
+	}
+}
